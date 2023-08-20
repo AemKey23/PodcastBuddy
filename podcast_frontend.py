12657a82deb410f8d602512fbbea0542de12eb1d
AemKey23 <140435800+AemKey23@users.noreply.github.com>
2023-08-20 13:08:54 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -6,6 +6,17 @@ import os
 def main():
     st.title("Newsletter Dashboard")
 
+    # Set the font and font size for the variable names and comments
+    st.set_option('font', 'Roboto')
+    st.set_option('font_size', 12)
+
+    # Use colors to highlight the different parts of the code
+    st.color_code(comments='light_blue', code='dark_blue')
+
+    # Use images and icons to represent the different steps of the process
+    image_magnifying_glass = st.image('https://iconfinder.com/icons/2408457/search_magnifier_magnifying_glass_loupe_icon')
+    image_check_mark = st.image('https://iconfinder.com/icons/2518105/check_tick_ok_success_icon')
+
     available_podcast_info = create_dict_from_json_files('.')
 
     # Left section - Input fields
@@ -13,7 +24,11 @@ def main():
 
     # Dropdown box
     st.sidebar.subheader("Available Podcasts Feeds")
-    selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
+    selected_podcast = st.sidebar.selectbox(
+        "Select Podcast",
+        options=available_podcast_info.keys(),
+        icon=image_magnifying_glass
+    )
 
     if selected_podcast:
 
@@ -24,7 +39,7 @@ def main():
 
         # Display the podcast title
         st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
+        st.write(podcast_info['podcast_details']['episode_title'], unsafe_allow_html=True)
 
         # Display the podcast summary and the cover image in a side-by-side layout
         col1, col2 = st.columns([7, 3])
@@ -32,21 +47,20 @@ def main():
         with col1:
             # Display the podcast episode summary
             st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
+            st.write(podcast_info['podcast_summary'], unsafe_allow_html=True)
 
         with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True, icon=image_magnifying_glass)
 
         # Display the podcast guest and their details in a side-by-side layout
         col3, col4 = st.columns([3, 7])
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            st.write(podcast_info['podcast_guest']['name'], unsafe_allow_html=True)
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['org'])
-        
+            st.write(podcast_info["podcast_guest"]['org'], unsafe_allow_html=True)
 
         # Display the five key moments
         st.subheader("Key Moments")
@@ -57,9 +71,9 @@ def main():
 
     # User Input box
     st.sidebar.subheader("Add and Process New Podcast Feed")
-    url = st.sidebar.text_input("Link to RSS Feed")
+    url = st.sidebar.text_input("Link to RSS Feed", icon=image_magnifying_glass)
 
-    process_button = st.sidebar.button("Process Podcast Feed")
+    process_button = st.sidebar.button("Process Podcast Feed", icon=image_check_mark)
     st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
 
     if process_button:
@@ -72,55 +86,7 @@ def main():
 
         # Display the podcast title
         st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
+        st.write(podcast_info['podcast_details']['episode_title'], unsafe_allow_html=True)
 
         # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
-
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
-
-def create_dict_from_json_files(folder_path):
-    json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
-    data_dict = {}
-
-    for file_name in json_files:
-        file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'r') as file:
-            podcast_info = json.load(file)
-            podcast_name = podcast_info['podcast_details']['podcast_title']
-            # Process the file data as needed
-            data_dict[podcast_name] = podcast_info
-
-    return data_dict
-
-def process_podcast_info(url):
-    f = modal.Function.lookup("corise-podcast-project", "process_podcast")
-    output = f.call(url, '/content/podcast/')
-    return output
-
-if __name__ == '__main__':
-    main()
+        col1
