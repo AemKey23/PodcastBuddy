3da6ee5f4a9ac218bd2d2c4e24e76d71509de68d
AemKey23 <140435800+AemKey23@users.noreply.github.com>
2023-08-20 16:21:54 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -45,7 +45,7 @@ def main():
             st.write(podcast_info['podcast_guest']['name'])
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['org'])
+            st.write(podcast_info["podcast_guest"]['summary'])
         
 
         # Display the five key moments
